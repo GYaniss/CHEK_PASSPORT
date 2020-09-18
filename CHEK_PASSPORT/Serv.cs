82d5c6a1b479700964bc86eb56f1469beb354ce5
GYaniss <yaniss@list.ru>
2020-09-18 23:36:46 +0500

    Оптимизация ресурсов

--- CHEK_PASSPORT/Serv.cs
+++ CHEK_PASSPORT/Serv.cs
@@ -12,9 +12,10 @@ namespace CHEK_PASSPORT
 {
     public class MainTask
     {
-        const int cacheBufferTime = 120; //Длительность хранения запроса в минутах в ОЗУ
-        public bool PoolHTTPSended = false;
-        private readonly ConcurrentQueue<string> Pool = new ConcurrentQueue<string>(); //Очередь невыполненных запросов
+        const   int cacheBufferTime = 120; //Длительность хранения запроса в минутах в ОЗУ
+        public  bool  PoolHTTPSended1 = false;
+        public  bool  PoolHTTPSended2 = false;
+        public  ConcurrentQueue<string> Pool = new ConcurrentQueue<string>(); //Очередь невыполненных запросов
         private readonly IMemoryCache MCache;
         private readonly VedomstvoHttpClient VedomstvoHttp;
         public MainTask(IMemoryCache mCache, VedomstvoHttpClient VedomstvoClient)
@@ -36,51 +37,44 @@ namespace CHEK_PASSPORT
             {
                 PD = new PassportData() { Ser = ser, Nom = nom, DateTime_Zapros = DateTime.UtcNow, Status = PassStatus.Registred };
                 MCache.Set(cacheKey, PD, TimeSpan.FromMinutes(cacheBufferTime));
-                Pool.Enqueue(cacheKey);
-                if (!PoolHTTPSended)
-                {
-                    PoolHTTPSended = true;
-                    Task.Run(() => SendPoolParralel());
-                }
+                Pool.Enqueue(cacheKey); 
+                if (!PoolHTTPSended1) {  PoolHTTPSended1 = true; Task.Run(() => SendPoolTask(ref PoolHTTPSended1)); }; //Запуск потока1 к web-api ведомства
+                if (!PoolHTTPSended2) {  PoolHTTPSended2 = true; Task.Run(() => SendPoolTask(ref PoolHTTPSended2)); }; //Запуск потока2 к web-api ведомства
                 return PD;
             }
         }
 
-        private void SendPoolParralel()
+        /// <summary>
+        /// Последовательный разбор общей очереди
+        /// </summary>
+        /// <param name="nomPool"></param>
+        private void SendPoolTask(ref bool PoolHTTPSended)
         {
-            Action HTTPSend = () =>
+            string cacheKey;
+            while (Pool.TryDequeue(out cacheKey))
+            {
+                PassportData PD;
+                if (MCache.TryGetValue(cacheKey, out PD))
                 {
-                    string cacheKey;
-                    while (Pool.TryDequeue(out cacheKey))
+                    PD.Status = PassStatus.Send_to_vedomsrvo;
+                    MCache.Set(cacheKey, PD);
+                    try
                     {
-                        PassportData PD;
-                        if (MCache.TryGetValue(cacheKey, out PD))
-                        {
-                            PD.Status = PassStatus.Send_to_vedomsrvo;
-                            MCache.Set(cacheKey, PD);
-                            try
-                            {
-                                PD.OtvetHTML = string.Join(";", VedomstvoHttp.GetProverkaPassportAsync(cacheKey).Result.AsEnumerable().ToArray());
-                                PD.Status = PassStatus.ОК;
-                                PD.DateTime_Otvet = DateTime.UtcNow;
-                            }
-                            catch (Exception Err)
-                            {
-                                PD.Status = PassStatus.Error;
-                                PD.OtvetHTML = Err.Message;
-                                PD.DateTime_Otvet = DateTime.UtcNow;
-                            }
-                            MCache.Set(cacheKey, PD);
-                        }
+                        var reply = VedomstvoHttp.GetProverkaPassportAsync(cacheKey);
+                        PD.OtvetHTML = string.Join(";", reply.Result.AsEnumerable().ToArray());
+                        PD.Status = PassStatus.ОК;
+                        PD.DateTime_Otvet = DateTime.UtcNow;
                     }
-                };
-            //количество одновременных соединений, разрешенных ведомством для обращения к своему API
-            //(3 для примера)
-            if (ServicePointManager.DefaultConnectionLimit < 3) 
-            { 
-                ServicePointManager.DefaultConnectionLimit = 3; };
-            Parallel.Invoke(HTTPSend, HTTPSend, HTTPSend);
-            PoolHTTPSended = false;
+                    catch (Exception Err)
+                    {
+                        PD.Status = PassStatus.Error;
+                        PD.OtvetHTML = Err.Message;
+                        PD.DateTime_Otvet = DateTime.UtcNow;
+                    }
+                    MCache.Set(cacheKey, PD);
+                }
+            }
+            PoolHTTPSended= false;
         }
     }
 
